2b1a17ea8a3cc3954f6c00bf633bb87cf0c29ec7
Marco Moschettini <m.marmos@gmail.com>
2019-03-30 20:37:55 +0100

    Add transactions and rewards

--- src/blockchain.js
+++ src/blockchain.js
@@ -1,19 +1,43 @@
 const {SHA256} = require('crypto-js');
 
+class Transaction
+{
+    constructor(from, to, amount)
+    {
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
+        this.timestamp = Date.now();
+    }
+
+    calculate_hash() 
+    {
+        return SHA256(this.from + this.to + this.amount + this.timestamp).toString();
+    }
+
+    is_valid()
+    {
+        if (this.from === null) 
+            return true;
+
+        return true;
+    }
+}
+
 class Block
 {
-    constructor(timestamp, data, previous_hash = '')
+    constructor(timestamp, transactions, previous_hash = '')
     {
         this.timestamp = timestamp;
         this.previous_hash = previous_hash;
-        this.data = data;
+        this.transactions = transactions;
         this.nonce = 0;
         this.hash = this.calculate_hash();
     }
 
     calculate_hash()
     {
-        return SHA256(this.timestamp + this.previous_hash + JSON.stringify(this.data) + this.nonce).toString();
+        return SHA256(this.timestamp + this.previous_hash + JSON.stringify(this.transactions) + this.nonce).toString();
     }   
 
     mine(difficulty)
@@ -26,19 +50,35 @@ class Block
     
         console.log('Block mined:', this.hash);
     }
+
+    has_valid_transactions() 
+    {
+        for (const transaction of this.transactions) 
+        {
+            if (!transaction.isValid())
+                return false;
+        }
+    
+        return true;
+    }
+    
 }
 
 class Blockchain
 {
-    constructor()
+    constructor(difficulty = 2)
     {
         this.chain = [this.create_genesis_block()];
-        this.difficulty = 2;
+        this.difficulty = difficulty;
+        this.pending_transactions = [];
+        this.mining_reward = 100;
+        
+        console.log('Blockchain created with difficulty:', this.difficulty, 'and mining reward:', this.mining_reward);
     }
     
     create_genesis_block()
     {
-        return new Block(Date.parse('2017-01-01'), {}, '0');
+        return new Block(Date.parse('2017-01-01'), [], '0');
     }
 
     get_last_block()
@@ -46,12 +86,44 @@ class Blockchain
         return this.chain[this.chain.length - 1];
     }
 
-    add_block(new_block)
+    add_transaction(transaction)
     {
-        new_block.previous_hash = this.get_last_block().hash;
+        if(!transaction.from || !transaction.to)
+            throw new Error('Transaction must include from and to address');
+        
+        if (!transaction.is_valid()) 
+            throw new Error('Cannot add invalid transaction to chain');
+        
+        this.pending_transactions.push(transaction);
+    }
+
+    mine_pending_transactions(reward_address)
+    {
+        let reward_transaction = new Transaction(null, reward_address, this.mining_reward)
+        this.pending_transactions.push(reward_transaction);
+
+        let new_block = new Block(Date.now(), this.pending_transactions, this.get_last_block().hash);
         new_block.mine(this.difficulty);
 
         this.chain.push(new_block);
+        this.pending_transactions = [];
+    }
+
+    get_balance(address)
+    {
+        let balance = 0;
+        for(const block of this.chain)
+        {
+            for(const transaction of block.transactions)
+            {
+                if(transaction.from === address)
+                    balance -= transaction.amount;
+
+                if(transaction.to === address)
+                    balance += transaction.amount;
+            }
+        }
+        return balance;
     }
 
     is_valid() 
@@ -65,6 +137,9 @@ class Blockchain
         {
             const current_block = this.chain[i];
             const previous_block = this.chain[i - 1];
+
+            if (!current_block.has_valid_transactions())
+                return false;
 
             if(current_block.hash !== current_block.calculate_hash()) 
                 return false;
@@ -82,4 +157,4 @@ class Blockchain
     }
 }
 
-module.exports = {Blockchain, Block};
+module.exports = {Blockchain, Block, Transaction};
--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
-const {Block, Blockchain} = require('./blockchain');
+const {Blockchain, Transaction} = require('./blockchain');
 
-let my_coin = new Blockchain();
+let my_coin = new Blockchain(4);
 
-console.log('Mining block 1');
-my_coin.add_block(new Block(Date.now(), {amount: 2}));
+my_coin.add_transaction(new Transaction('address1', 'address2', 5));
+my_coin.add_transaction(new Transaction('address1', 'address2', 50));
+my_coin.add_transaction(new Transaction('address2', 'address1', 25));
 
-console.log('Mining block 2');
-my_coin.add_block(new Block(Date.now(), {amount: 5}));
+my_coin.mine_pending_transactions('miner');
+
+console.log('Address 1 balance', my_coin.get_balance('address1'));
+console.log('Address 2 balance', my_coin.get_balance('address2'));
+console.log('Miner balance', my_coin.get_balance('miner'));
