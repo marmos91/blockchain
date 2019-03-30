11e0b714e5a34834f565db648ce9a460b9078196
Marco Moschettini <m.marmos@gmail.com>
2019-03-30 20:16:22 +0100

    Minor changes

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ const {Block, Blockchain} = require('./blockchain');
 let my_coin = new Blockchain();
 
 console.log('Mining block 1');
-my_coin.add_block(new Block(Date.now(), {name: 'Marco'}));
+my_coin.add_block(new Block(Date.now(), {amount: 2}));
 
 console.log('Mining block 2');
-my_coin.add_block(new Block(Date.now(), {surname: 'Moschettini'}));
+my_coin.add_block(new Block(Date.now(), {amount: 5}));
